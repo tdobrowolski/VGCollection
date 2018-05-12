747eae49dec93f2be47c0d5c99d77b911f12afc8
tdobrowolski <tobdob15@gmail.com>
2018-05-12 22:13:01 +0200

    Initial Commit

---
+++ VGCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VGCollection
+//
+//  Created by Tobiasz Dobrowolski on 12.05.2018.
+//  Copyright © 2018 Tobiasz Dobrowolski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
